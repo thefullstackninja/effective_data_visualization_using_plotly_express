fb82805936f68b346293646e56bd6521f39652c1
olabodealamu <bodealamu007@gmail.com>
2020-12-05 11:32:48 -0600

    scatterplots

---
+++ scatterplots/basic_iris_scatterplot_facets.py
@@ -0,0 +1,16 @@
+import plotly.express as px
+import pandas as pd
+
+# import dataset
+df = pd.read_csv("../data/iris.csv")
+
+print(df.columns)
+
+scatterplot = px.scatter(data_frame=df,
+                         x='SepalWidthCm',
+                         y='PetalLengthCm',
+                         size='PetalWidthCm',
+                         title="Plot of Sepal width vs Petal length for the Iris flower using Petal width as the size parameter",
+                         facet_col='Species')
+
+scatterplot.show()
